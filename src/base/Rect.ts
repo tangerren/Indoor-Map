44f01b56213a708df939898351cea2035ffec581
WoShiSunWuKong <1026435687@qq.com>
2018-03-29 22:43:46 +0800

    add 'Rect' type

--- src/base/Rect.ts
+++ src/base/Rect.ts
@@ -1,19 +1,6 @@
-export class Rect {
-
-	tl: Array<number> = [0.0];
-	br: Array<number> = [0.0];
-
-	constructor(minx?: number, miny?: number, maxx?: number, maxy?: number) {
-		this.tl = [minx || 0, miny || 0]; //top left point
-		this.br = [maxx || 0, maxy || 0]; //bottom right point
-	}
-
-	isCollide(rect: Rect): boolean {
-		if (rect.br[0] < this.tl[0] || rect.tl[0] > this.br[0] ||
-			rect.br[1] < this.tl[1] || rect.tl[1] > this.br[1]) {
-			return false;
-		}
-		return true;
-	}
-
+export interface Rect {
+	arrVector2: Array<THREE.Vector2>;
+	floor: number;
+	height: number;
+	center: Array<number>;
 }
